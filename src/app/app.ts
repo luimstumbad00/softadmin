f92a68e20482d5d31d8bce1b5763c9ebedb81d85
luimstumbad00 <aguilar.torres.luis.06@gmail.com>
2025-07-20 07:44:24 -0600

    solo creacion del proyecto

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Footer } from "./components/footer/footer";
+import { Header } from "./components/header/header";
+import { Login } from "./components/login/login";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Footer, Header, Login],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
